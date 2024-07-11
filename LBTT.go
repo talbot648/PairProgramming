a859747018d981edbde57679f169f6664ccce0f9
Charles Talbot <charles.talbot@bjss.com>
2024-07-11 15:18:01 +0100

    added acceptance test code and first acceptance test

--- LBTT.go
+++ LBTT.go
@@ -4,6 +4,76 @@ import (
 	"errors"
 )
 
+type TaxInfo struct {
+	startOfTaxBand            int
+	taxRate                   int
+	totalTaxFromPreviousBands int
+}
+
+var taxInformation = []TaxInfo{
+	{startOfTaxBand: 0, taxRate: 0, totalTaxFromPreviousBands: 0},
+	{startOfTaxBand: 145000, taxRate: 2, totalTaxFromPreviousBands: 0},
+	{startOfTaxBand: 250000, taxRate: 5, totalTaxFromPreviousBands: 2100},
+	{startOfTaxBand: 325000, taxRate: 10, totalTaxFromPreviousBands: 5850},
+	{startOfTaxBand: 750000, taxRate: 12, totalTaxFromPreviousBands: 48350},
+}
+
+func CalculateLBTT(housePrice int) (int, error) {
+	var totalTax int
+	if !isPriceValid(housePrice) {
+		return 0, errors.New("invalid input: cannot have a house price at zero pounds or below")
+	}
+	if housePrice <= 145000 {
+		return totalTax, nil
+	}
+	taxBand := getTaxBandIndex(housePrice)
+	return taxBand.calculateTax(housePrice), nil
+	/*
+		if isPriceInFirstTaxBand(housePrice) {
+			totalTax := calculateFirstTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+		if isPriceInSecondTaxBand(housePrice) {
+			totalTax := calculateSecondTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+		if isPriceInThirdTaxBand(housePrice) {
+			totalTax := calculateThirdTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+		if isPriceInHighestTaxBand(housePrice) {
+			totalTax := calculateHighestTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+	*/
+
+}
+
+func (t *TaxInfo) calculateTax(housePrice int) int {
+	totalToBeTaxed := housePrice - t.startOfTaxBand
+
+	totalCurrentBandTax := totalToBeTaxed * t.taxRate / 100
+	totalTax := totalCurrentBandTax + t.totalTaxFromPreviousBands
+
+	return totalTax
+}
+
+func getTaxBandIndex(housePrice int) TaxInfo {
+	for i := len(taxInformation) - 1; i >= 0; i-- {
+		if housePrice > taxInformation[i].startOfTaxBand {
+			return taxInformation[i]
+		}
+	}
+	return taxInformation[0]
+}
+
+func isPriceValid(housePrice int) bool {
+	return housePrice > 0
+}
+
+func main() {
+}
+
 /*
 func calculateFirstTaxBandTotal(housePrice int) int {
 	startOfTaxBand := 145000
@@ -53,76 +123,6 @@ func calculateHighestTaxBandTotal(housePrice int) int {
 }
 
 */
-
-type TaxInfo struct {
-	startOfTaxBand            int
-	taxRate                   int
-	totalTaxFromPreviousBands int
-}
-
-var taxInformation = []TaxInfo{
-	{startOfTaxBand: 0, taxRate: 0, totalTaxFromPreviousBands: 0},
-	{startOfTaxBand: 145000, taxRate: 2, totalTaxFromPreviousBands: 0},
-	{startOfTaxBand: 250000, taxRate: 5, totalTaxFromPreviousBands: 2100},
-	{startOfTaxBand: 325000, taxRate: 10, totalTaxFromPreviousBands: 5850},
-	{startOfTaxBand: 750000, taxRate: 12, totalTaxFromPreviousBands: 48350},
-}
-
-func CalculateLBTT(housePrice int) (int, error) {
-	var totalTax int
-	if !isPriceValid(housePrice) {
-		return 0, errors.New("invalid input: cannot have a house price at zero pounds or below")
-	}
-	if housePrice <= 145000 {
-		return totalTax, nil
-	}
-	taxBand := getTaxBandIndex(housePrice)
-	return taxBand.calculateTax(housePrice), nil
-	/*
-		if isPriceInFirstTaxBand(housePrice) {
-			totalTax := calculateFirstTaxBandTotal(housePrice)
-			return totalTax, nil
-		}
-		if isPriceInSecondTaxBand(housePrice) {
-			totalTax := calculateSecondTaxBandTotal(housePrice)
-			return totalTax, nil
-		}
-		if isPriceInThirdTaxBand(housePrice) {
-			totalTax := calculateThirdTaxBandTotal(housePrice)
-			return totalTax, nil
-		}
-		if isPriceInHighestTaxBand(housePrice) {
-			totalTax := calculateHighestTaxBandTotal(housePrice)
-			return totalTax, nil
-		}
-	*/
-
-}
-
-func main() {
-}
-
-func (t *TaxInfo) calculateTax(housePrice int) int {
-	totalToBeTaxed := housePrice - t.startOfTaxBand
-
-	totalCurrentBandTax := totalToBeTaxed * t.taxRate / 100
-	totalTax := totalCurrentBandTax + t.totalTaxFromPreviousBands
-
-	return totalTax
-}
-
-func getTaxBandIndex(housePrice int) TaxInfo {
-	for i := len(taxInformation) - 1; i >= 0; i-- {
-		if housePrice > taxInformation[i].startOfTaxBand {
-			return taxInformation[i]
-		}
-	}
-	return taxInformation[0] // Default to the first band if no match is found
-}
-
-func isPriceValid(housePrice int) bool {
-	return housePrice > 0
-}
 
 /*
 
--- renamedLBTT_test.go
+++ renamedLBTT_test.go
@@ -113,3 +113,29 @@ func TestCalculateHighestTaxBand(t *testing.T) {
 		t.Errorf("got %v, expected %v", got, want)
 	}
 }
+
+func TestAcceptanceTests(t *testing.T) {
+	tests := []struct {
+		name        string
+		housePrice  int
+		expected    int
+		expectedErr error
+	}{
+		{name: "Invalid House Price Below Zero", housePrice: -5, expected: 0, expectedErr: errors.New("invalid input: cannot have a house price at zero pounds or below")},
+	}
+
+	for _, test := range tests {
+
+		got, err := CalculateLBTT(test.housePrice)
+
+		if test.expectedErr != nil {
+			if err == nil {
+				t.Errorf("%s (%v) Expected error, but received nil", test.name, test.housePrice)
+			} else if err.Error() != test.expectedErr.Error() {
+				t.Errorf("%s (%v) got error %v, want %v", test.name, test.housePrice, err, test.expectedErr)
+			}
+		} else if got != test.expected {
+			t.Errorf("%s (%v) got %v, want %v", test.name, test.housePrice, got, test.expected)
+		}
+	}
+}
