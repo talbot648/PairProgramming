5e855b9fba7f0c38bec18823b858c6ed91f1da78
Charles Talbot <charles.talbot@bjss.com>
2024-07-11 14:02:34 +0100

    reformatted prod code to add a tax info struct and a slice of type struct to make the code more expandable

--- LBTT.go
+++ LBTT.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 )
 
+/*
 func calculateFirstTaxBandTotal(housePrice int) int {
 	startOfTaxBand := 145000
 	taxRate := 2 //percent
@@ -51,7 +52,20 @@ func calculateHighestTaxBandTotal(housePrice int) int {
 	return totalTax
 }
 
-func main() {
+*/
+
+type TaxInfo struct {
+	startOfTaxBand            int
+	taxRate                   int
+	totalTaxFromPreviousBands int
+}
+
+var taxInformation = []TaxInfo{
+	{startOfTaxBand: 0, taxRate: 0, totalTaxFromPreviousBands: 0},
+	{startOfTaxBand: 145000, taxRate: 2, totalTaxFromPreviousBands: 0},
+	{startOfTaxBand: 250000, taxRate: 5, totalTaxFromPreviousBands: 2100},
+	{startOfTaxBand: 325000, taxRate: 10, totalTaxFromPreviousBands: 5850},
+	{startOfTaxBand: 750000, taxRate: 12, totalTaxFromPreviousBands: 48350},
 }
 
 func CalculateLBTT(housePrice int) (int, error) {
@@ -62,30 +76,55 @@ func CalculateLBTT(housePrice int) (int, error) {
 	if housePrice <= 145000 {
 		return totalTax, nil
 	}
+	taxBand := getTaxBandIndex(housePrice)
+	return taxBand.calculateTax(housePrice), nil
+	/*
+		if isPriceInFirstTaxBand(housePrice) {
+			totalTax := calculateFirstTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+		if isPriceInSecondTaxBand(housePrice) {
+			totalTax := calculateSecondTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+		if isPriceInThirdTaxBand(housePrice) {
+			totalTax := calculateThirdTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+		if isPriceInHighestTaxBand(housePrice) {
+			totalTax := calculateHighestTaxBandTotal(housePrice)
+			return totalTax, nil
+		}
+	*/
 
-	if isPriceInFirstTaxBand(housePrice) {
-		totalTax := calculateFirstTaxBandTotal(housePrice)
-		return totalTax, nil
-	}
-	if isPriceInSecondTaxBand(housePrice) {
-		totalTax := calculateSecondTaxBandTotal(housePrice)
-		return totalTax, nil
-	}
-	if isPriceInThirdTaxBand(housePrice) {
-		totalTax := calculateThirdTaxBandTotal(housePrice)
-		return totalTax, nil
-	}
-	if isPriceInHighestTaxBand(housePrice) {
-		totalTax := calculateHighestTaxBandTotal(housePrice)
-		return totalTax, nil
-	}
+}
 
-	return 0, errors.New("")
+func main() {
+}
+
+func (t *TaxInfo) calculateTax(housePrice int) int {
+	totalToBeTaxed := housePrice - t.startOfTaxBand
+
+	totalCurrentBandTax := totalToBeTaxed * t.taxRate / 100
+	totalTax := totalCurrentBandTax + t.totalTaxFromPreviousBands
+
+	return totalTax
+}
+
+func getTaxBandIndex(housePrice int) TaxInfo {
+	for i := len(taxInformation) - 1; i >= 0; i-- {
+		if housePrice > taxInformation[i].startOfTaxBand {
+			return taxInformation[i]
+		}
+	}
+	return taxInformation[0] // Default to the first band if no match is found
 }
 
 func isPriceValid(housePrice int) bool {
 	return housePrice > 0
 }
+
+/*
 
 func isPriceInFirstTaxBand(housePrice int) bool {
 	return housePrice > 145000 && housePrice <= 250000
@@ -102,3 +141,4 @@ func isPriceInThirdTaxBand(housePrice int) bool {
 func isPriceInHighestTaxBand(housePrice int) bool {
 	return housePrice > 750000
 }
+*/
